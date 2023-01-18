4ee3641e52ecfe13c19c036d05ae43fbb8f88c45
Jiangxianzhi <772423266@qq.com>
2023-01-18 15:01:58 +0800

    add my study

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2022 Start Bootstrap
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
 //
@@ -13,10 +13,11 @@ window.addEventListener('DOMContentLoaded', event => {
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
-            return;
+            return;//如果找不到就返回
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+        if (window.scrollY === 0) { //window.scrollY垂直滚动值
+            navbarCollapsible.classList.remove('navbar-shrink') //取消掉这个class
+            // navbar-shrink 效果是变黑，变窄
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
@@ -30,14 +31,15 @@ window.addEventListener('DOMContentLoaded', event => {
     document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
+    // bootstrap.scrollspy 监听，到对应的区域，但是为什么会变蓝呢？
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
             offset: 74,
-        });
+        });//offset == 偏移量
     };
-
+// 宽度变短，收缩
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
